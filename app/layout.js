9a40ce16832468b7f81642f63bf21358742b31e2
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-03-02 17:58:10 +0330

    Update layout and components structure, fix logo path issue

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,13 @@
-import Logo from "@/app/_components/Logo"
-import Navgation from "@/app/_components/Navigation"
+
+import {Josefin_Sans} from "next/font/google"
+ const josefun = Josefin_Sans({
+  subsets : ["latin"],
+  display : "swap",
+  
+})
+console.log(josefun);
 import "@/app/_styles/globals.css"
+import Header from "./_components/Header";
 export const metadata = {
   title : {
     template: "%s / The Wild Oasis",
@@ -13,15 +20,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-      <header>
-      <Logo />
-      <Navgation />
-      </header>
-      <main>
+      <body className={`${josefun.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+     <Header/> 
+     <div className="flex-1 px-8 py-12">
+
+      <main className="max-w-7xl mx-auto ">
       {children}
       </main>
-      <footer>Copyright by The Wild Oasis</footer>
+     </div>
+      {/* <footer>Copyright by The Wild Oasis</footer> */}
       </body>
     </html>
   )
