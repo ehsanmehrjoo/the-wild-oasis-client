b51ce3a1a8c1ca2e88e501d5593b6445b48d728e
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-03-18 13:12:24 +0330

    Updated

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,29 +1,17 @@
-"use client";
+import CabinCard from "@/app/_components/CabinCard"
+import { getCabins } from "../_lib/data-service";
 
-import { useQuery } from '@tanstack/react-query';
- 
-import CabinCard from "@/app/_components/CabinCard";
 
-async function fetchCabins() {
-  const res = await fetch('/api/cabins');
-  if (!res.ok) throw new Error('Failed to fetch cabins');
-  return res.json();
-}
-
-function CabinList() {
-  const { data: cabins, error, isLoading } = useQuery(['cabins'], fetchCabins, {
-    refetchInterval: 5000 // آپدیت هر 5 ثانیه
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-  if (!cabins?.length) return <p>No cabins available.</p>;
-
+async function  CabinList() {
+  const cabins = await getCabins();
+  if(!cabins.length) return null
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
-        <CabinCard cabin={cabin} key={cabin.id} />
-      ))}
-    </div>
-  );
+    {cabins.map((cabin) => (
+      <CabinCard cabin={cabin} key={cabin.id} />
+    ))}
+  </div>
+  )
 }
+
+export default CabinList
