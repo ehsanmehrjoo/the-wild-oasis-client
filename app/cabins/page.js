5324ab300529c6972411ae1ea8d08da89dcd6d98
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-03-23 12:31:51 +0330

    Implement cabin filter feature with active state management using URL search params and conditional rendering for button styles

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -4,13 +4,16 @@ import { getCabins } from "@/app/_lib/data-service";
 import CabinList from "../_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 86400
 export const metadata = {
     title: 'Cabins',
   }
 
-export default  function Page() {
+export default  function Page({searchParams}) {
+  console.log(searchParams);
+  const filter = searchParams?.capacity ?? "all"
    return (
      <div>
        <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,10 +27,13 @@ export default  function Page() {
          away from home. The perfect spot for a peaceful, calm vacation. Welcome
          to paradise.
        </p>
- 
-      <Suspense fallback={<Spinner />}>
+       <div className="flex justify-end mb-8">
 
-        <CabinList />
+       <Filter />
+       </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+
+        <CabinList  filter={filter}/>
       </Suspense>
        
      </div>)
