742b4200cc9e4f2f9cf67e5bd61a834d4d5a286b
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-04-19 10:49:51 +0330

    Reorder mobile nav: UserMenuItem is now the first item

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -10,6 +10,7 @@ export default function Navigation() {
 
   return (
     <nav className="z-10 text-lg relative">
+      {/* دکمه منوی همبرگری فقط برای موبایل */}
       <button
         className="md:hidden p-2 text-primary-100"
         onClick={() => setIsOpen(!isOpen)}
@@ -17,12 +18,16 @@ export default function Navigation() {
         {isOpen ? <X size={28} /> : <Menu size={28} />}
       </button>
 
+      {/* منوی موبایل */}
       <div
         className={`fixed top-0 left-0 h-full w-64 bg-primary-900 shadow-lg p-6 transition-transform duration-300 z-50 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } md:hidden`}
       >
         <ul className="flex flex-col gap-6 mt-12">
+          <li onClick={() => setIsOpen(false)}>
+            <UserMenuItem />
+          </li>
           <li>
             <Link href="/" onClick={() => setIsOpen(false)}>
               Home
@@ -38,12 +43,10 @@ export default function Navigation() {
               About
             </Link>
           </li>
-          <li onClick={() => setIsOpen(false)}>
-            <UserMenuItem />
-          </li>
         </ul>
       </div>
 
+      {/* منوی دسکتاپ */}
       <ul className="hidden md:flex gap-12 items-center">
         <li>
           <Link href="/cabins" className="hover:text-accent-400 transition-colors">
